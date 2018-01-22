d85c5a3c4d271065648294817c437d450911c516
SAMII-PC\SAM II <nishokvg@hotmail.com>
2018-01-21 21:04:44 -0600

    second commit

--- src/Prime/Prime.java
+++ src/Prime/Prime.java
@@ -2,7 +2,7 @@ package Prime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-
+// main program
 public class Prime {
 
     public static void main (String[] args){
@@ -11,10 +11,11 @@ public class Prime {
         int status = 1;
         int num = 3;
         List<Integer> Primenumber = new ArrayList<Integer>();
-       
+   //getting the n prime number as input    
         System.out.println("Enter N Integers:");
         Scanner inputNumber = new Scanner(System.in);
         N = inputNumber.nextInt();
+        // calculating the N prime number
         Primenumber.add(2);
         for ( int a = 2 ; a <=N ;  )
         {
@@ -34,6 +35,8 @@ public class Prime {
             status = 1;
             num++;
         }
+        
+        // printing n prime number in row and 10 multiplication on column
         for(int i=1;i<=10;i++){
             for(int j=0;j<Primenumber.size();j++) {
                 System.out.print(Primenumber.get(j) * i +"    ");
